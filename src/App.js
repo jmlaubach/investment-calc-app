891b030bc37b4492b5f20d021d20cabbe5c52251
joshua.m.laubach <laubachj@leidos.com>
2023-09-05 10:51:02 -0400

    Add functionality to reset the table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import logo from './assets/investment-calculator-logo.png';
 import InvestmentForm from './components/Form/InvestmentForm';
 import ResultTable from './components/Table/ResultTable';
 
-function App() {
+function App(props) {
+  const [isFormReset, setIsFormReset] = useState(true);
+
+  const resetFormHandler = (event) => {
+    if (isFormReset) {
+      setIsFormReset(false);
+    } else {
+      setIsFormReset(true);
+    }
+  };
+
   return (
     <div>
       <header className="header">
         <img src={logo} alt="logo" />
         <h1>Investment Calculator</h1>
       </header>
-      <InvestmentForm />
-
-      <ResultTable />
+      <InvestmentForm resetForm={resetFormHandler}/>
+      {isFormReset ? (
+        <p textAlign="center">No investment calculated yet.</p>
+      ) : (
+        <ResultTable />
+      )}
 
       {/* Todo: Show below table conditionally (only once result data is available) */}
       {/* Show fallback text if no data is available */}
