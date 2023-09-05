134bc8d9f2c41524c64cb1c5d9fb991ddc89ba34
joshua.m.laubach <laubachj@leidos.com>
2023-09-05 10:32:29 -0400

    Split app into separate components

---
+++ src/components/Table/ResultTable.js
@@ -0,0 +1,21 @@
+import TableRow from "./TableRow";
+
+function ResultTable() {
+
+  return (
+    <table className="result">
+      <thead>
+        <tr>
+          <th>Year</th>
+          <th>Total Savings</th>
+          <th>Interest (Year)</th>
+          <th>Total Interest</th>
+          <th>Invested Capital</th>
+        </tr>
+      </thead>
+      <TableRow />
+    </table>
+  );
+}
+
+export default ResultTable;
